daafbb729436a4a099a6cfeed0cecfbed02863f6
Menachem Glik <menachemglik@yahoo.com>
2020-07-10 17:51:58 -0400

    Display news archive

--- src/app/news/news-archive/news-archive.component.ts
+++ src/app/news/news-archive/news-archive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BlogApiService } from '../../shared/services/blog-api.service';
+
 @Component({
   selector: 'app-news-archive',
   templateUrl: './news-archive.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsArchiveComponent implements OnInit {
 
-  constructor() { }
+  constructor(private blogApi: BlogApiService) { }
+
+  public blogPosts: any[]; // Gets the blog's posts from API
+
+  getBlogPosts(): void {
+    this.blogApi.getDatafromBlog().subscribe(newsItems => {
+      this.blogPosts = newsItems;
+      //console.log( this.blogPosts);
+    })
+  }
 
   ngOnInit(): void {
+    this.getBlogPosts();
   }
 
 }
